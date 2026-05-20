ca667be15f511f0133c91ca68904a994fb82f317
VandanaVj <vandanavj287@gmail.com>
2026-05-20 21:57:13 +0530

    fixed imports in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-import productReducer from '../features/products/productSlice'
+import authReducer from '../Features/auth/authSlice'
+import productReducer from '../Features/products/productSlice'
 
 export const store = configureStore({
   reducer: {
